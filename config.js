b16ec438944e330da52312f64da03c8d0fbea5c8
evicom <93210079+evicom@users.noreply.github.com>
2026-08-26 21:42:29 -0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// ⚠️ ATENÇÃO: Este arquivo contém suas credenciais pessoais!
-// NÃO publique este arquivo no GitHub!
-// Ele já está no .gitignore para sua segurança.
-
-const CLOUD_CONFIG = {
-  binId: '6a8f816cda38895dfe15b0d6',
-  masterKey: '$2a$10$8Wm2Ixc0mRA38i0rNyecw.HFCnENmYPaEOnz6iIeon0uyxDxgzHAS',
-  baseUrl: 'https://api.jsonbin.io/v3/b'
-};
